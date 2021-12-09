94274ff39d7f75c568adc3684e8046d524a15d61
zunggzing <hello.trungvinh@gmail.com>
2021-12-09 08:10:19 +0700

    Protected route

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+  console.log(authCtx);
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -11,36 +15,48 @@ const MainNavigation = () => {
       </Link>
       <nav className={classes.nav}>
         <ul>
-          <li>
-            <NavLink to="/books" activeClassName={classes.active}>
-              List Books
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/new-book" activeClassName={classes.active}>
-              Add Book
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/authors" activeClassName={classes.active}>
-              List Authors
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/new-author" activeClassName={classes.active}>
-              Add Author
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/auth" activeClassName={classes.active}>
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/logout" activeClassName={classes.active}>
-              Logout
-            </NavLink>
-          </li>
+          {authCtx.isLoggedIn && (
+            <li>
+              <NavLink to="/books" activeClassName={classes.active}>
+                List Books
+              </NavLink>
+            </li>
+          )}
+          {authCtx.isLoggedIn && (
+            <li>
+              <NavLink to="/new-book" activeClassName={classes.active}>
+                Add Book
+              </NavLink>
+            </li>
+          )}
+          {authCtx.isLoggedIn && (
+            <li>
+              <NavLink to="/authors" activeClassName={classes.active}>
+                List Authors
+              </NavLink>
+            </li>
+          )}
+          {authCtx.isLoggedIn && (
+            <li>
+              <NavLink to="/new-author" activeClassName={classes.active}>
+                Add Author
+              </NavLink>
+            </li>
+          )}
+          {!authCtx.isLoggedIn && (
+            <li>
+              <NavLink to="/auth" activeClassName={classes.active}>
+                Login
+              </NavLink>
+            </li>
+          )}
+          {authCtx.isLoggedIn && (
+            <li>
+              <NavLink to="/logout" activeClassName={classes.active}>
+                Logout
+              </NavLink>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
