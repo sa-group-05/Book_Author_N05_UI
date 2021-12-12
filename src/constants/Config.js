af13c2928472961671f7ac173722b6d999a85dc3
zunggzing <hello.trungvinh@gmail.com>
2021-12-12 11:41:33 +0700

    Change URL to online - only authors - waiting apigetway books

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,1 +1,3 @@
-export const URL = "http://localhost:9191";
+// export const URL = "http://localhost:9191";
+export const URL =
+  "http://ec2-18-141-8-154.ap-southeast-1.compute.amazonaws.com:9191";
